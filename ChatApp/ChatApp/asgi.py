43d8ac9828024a8d35635d23b1e14e4775c92f93
Subham-WH-GT <awandboy685@gmail.com>
2024-07-05 12:38:26 +0530

    updated

--- ChatApp/ChatApp/asgi.py
+++ ChatApp/ChatApp/asgi.py
@@ -9,16 +9,25 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 
 import os
 
-from channels.routing import ProtocolTypeRouter
+from channels.routing import ProtocolTypeRouter,URLRouter
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ChatApp.settings')
+
+from channels.auth import AuthMiddlewareStack
+from chat import routing
+# from channels.routing import ProtocolTypeRouter
 
 django_asgi_app = get_asgi_application()
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE","ChatApp.settings")
 application=ProtocolTypeRouter({
-    "http":django_asgi_app,
+    "http":get_asgi_application(),
+    "websocket":AuthMiddlewareStack(
+        URLRouter(
+            routing.websocket_urlpatterns
+        )
+    )
 })
 
-ASGI_APPLICATION='ChatApp.asgi.application'
+# ASGI_APPLICATION='ChatApp.asgi.application'
--- ChatApp/ChatApp/settings.py
+++ ChatApp/ChatApp/settings.py
@@ -130,3 +130,9 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL="chat-page"
+
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels.layers.InMemoryChannelLayer",
+    }
+}
